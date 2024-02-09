8ce10a7d3ba57749278ad483bcdb519b124e46c4
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-09 17:39:09 +0530

    testing gitlab

---
+++ Jenkinsfile
@@ -0,0 +1,35 @@
+pipeline {
+
+    agent any
+
+    environment {
+        clusterName = "roboshop-eks-cluster-demo"
+        awsRegion = "us-east-1"
+    }
+
+    stages {
+        stage('Import EKS Cluster') {
+            steps {
+                withAWS(credentials: 'awsCreds', region: 'us-east-1') {
+                    sh "aws eks update-kubeconfig --region ${awsRegion} --name ${clusterName}"
+                }
+            }
+        }
+        stage('Deploy ArgoCD') {
+            steps {
+                withAWS(credentials: 'awsCreds', region: 'us-east-1') {
+                    sh "sh install.sh"
+                }
+            }
+        }
+    }
+    options {
+        preserveStashes()
+        timestamps()
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# link https://argo-cd.readthedocs.io/en/stable/getting_started/#1-install-argo-cd
+
+echo "checking gitlab status"
+helm ls -n gitlab | grep gitlab
+
+if [ $? -eq 0 ]; then
+  echo "gitlab installed already, trying to upgrade gitlab."
+  helm upgrade gitlab gitlab/gitlab -n gitlab --create-namespace=true --values values.yml
+  PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password --ojsonpath=’{.data.password}’)
+  echo "gitlab Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)"
+  else
+    echo "gitlab installing"
+    helm repo add gitlab https://charts.gitlab.io/
+    helm repo update
+    helm install gitlab gitlab/gitlab -n gitlab --create-namespace=true --values values.yml
+    sleep 10
+    echo "gitlab installed successfully"
+    PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password --ojsonpath=’{.data.password}’)
+    echo "gitlab Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)"
+fi
+
