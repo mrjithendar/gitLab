d6d083a73add75339d61e31feb378d1855dff547
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-09 18:29:46 +0530

    testing gitlab

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ helm ls -n gitlab | grep gitlab
 
 if [ $? -eq 0 ]; then
   echo "gitlab installed already, trying to upgrade gitlab."
-  helm upgrade gitlab gitlab/gitlab -n gitlab --create-namespace=true --values values.yml
+  helm upgrade gitlab gitlab/gitlab -n gitlab --create-namespace=true #--values values.yml
   PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password --ojsonpath=’{.data.password}’)
   echo "gitlab Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)"
   else
