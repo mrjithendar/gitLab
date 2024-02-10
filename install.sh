e918e4568519eb79a39708808844ac62c6600e37
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-10 12:53:19 +0530

    gitlab

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# link https://argo-cd.readthedocs.io/en/stable/getting_started/#1-install-argo-cd
+# link https://docs.gitlab.com/charts/quickstart/
 
 echo "checking gitlab status"
 helm ls -n gitlab | grep gitlab
 
 if [ $? -eq 0 ]; then
   echo "gitlab installed already, trying to upgrade gitlab."
-  helm upgrade gitlab gitlab/gitlab -n gitlab --create-namespace=true #--values values.yml
+  helm upgrade gitlab gitlab/gitlab --set global.hosts.domain=gitlab.decodedevops.com \
+       --set certmanager-issuer.email=team@decodedevops.com --set global.edition=ce
+  echo "gitlab upgraded successfully"
+  kubectl get svc -l app=nginx-ingress
   PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password --ojsonpath=’{.data.password}’)
   echo "gitlab Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)"
   else
     echo "gitlab installing"
     helm repo add gitlab https://charts.gitlab.io/
     helm repo update
-    helm install gitlab gitlab/gitlab -n gitlab --create-namespace=true --values values.yml
+    helm install gitlab gitlab/gitlab --set global.hosts.domain=gitlab.decodedevops.com \
+         --set certmanager-issuer.email=team@decodedevops.com --set global.edition=ce
     sleep 10
     echo "gitlab installed successfully"
+    kubectl get svc -l app=nginx-ingress
+    
     PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password --ojsonpath=’{.data.password}’)
     echo "gitlab Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)"
 fi
