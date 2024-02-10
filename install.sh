a2fed1cd0df4f2cd046afa9d9a216117c194803e
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-10 13:30:35 +0530

    gitlab

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@
 # link https://docs.$APP.com/charts/quickstart/
 
 APP="gitlab"
+DOMAIN="roboshop.com"
 
 echo "checking $APP status"
 helm ls -n $APP | grep $APP
 
 if [ $? -eq 0 ]; then
   echo "$APP installed already, trying to upgrade $APP."
-  helm upgrade $APP $APP/$APP -n $APP --set global.hosts.domain=decodedevops.com \
-       --set certmanager-issuer.email=team@decodedevops.com --set global.edition=ce --create-namespace
+  helm upgrade $APP $APP/$APP -n $APP --set global.hosts.domain=$DOMAIN \
+       --set certmanager-issuer.email=team@$DOMAIN --set global.edition=ce --create-namespace
   echo "$APP upgraded successfully"
   kubectl get svc -l app=nginx-ingress -n $APP
   PASSWORD=$(kubectl get secret $APP-$APP-initial-root-password -o jsonpath='{.data.password}' -n $APP)
@@ -19,8 +20,8 @@ if [ $? -eq 0 ]; then
     echo "$APP installing"
     helm repo add $APP https://charts.$APP.io/
     helm repo update
-    helm install $APP $APP/$APP -n $APP --set global.hosts.domain=decodedevops.com \
-         --set certmanager-issuer.email=team@decodedevops.com --set global.edition=ce --create-namespace
+    helm install $APP $APP/$APP -n $APP --set global.hosts.domain=$DOMAIN \
+         --set certmanager-issuer.email=team@$DOMAIN --set global.edition=ce --create-namespace
     sleep 10
     echo "$APP installed successfully"
     kubectl get svc -l app=nginx-ingress -n $APP
