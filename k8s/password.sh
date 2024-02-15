9ed9dba0dbb951f88cdbdc72c44c0aa23b78f442
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-15 14:29:14 +0530

    helm fix

--- k8s/password.sh
+++ k8s/password.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PASSWORD=$(kubectl get secret $APP-$APP-initial-root-password -o jsonpath='.data.password' -n $APP)
-echo '$APP Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)'
+echo $APP Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)
