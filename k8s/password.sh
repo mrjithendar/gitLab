323069f0b8401f535f3f02b731df365078f1871c
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-15 14:31:08 +0530

    helm fix

--- k8s/password.sh
+++ k8s/password.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+APP="gitlab"
+
 PASSWORD=$(kubectl get secret $APP-$APP-initial-root-password -o jsonpath='{.data.password}' -n $APP)
 echo "$APP Credentials Username: admin and Password is: $(echo $PASSWORD | base64 --decode)"
