6bb3fa4554c14bb75b0c5ecb503a9475636ada19
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2024-02-20 12:20:06 +0530

    generic commit

--- runner/runner.sh
+++ runner/runner.sh
@@ -1,8 +1,9 @@
 # https://docs.gitlab.com/runner/install/kubernetes.html
 
-kubectl create secret generic s3access \
-    --from-literal=accesskey="" \
-    --from-literal=secretkey="" -n gitlab
+GITLABRUNNERKEY=
+# kubectl create secret generic s3access \
+#     --from-literal=accesskey="" \
+#     --from-literal=secretkey="" -n gitlab
 
 
 helm repo add gitlab https://charts.gitlab.io
@@ -13,14 +14,10 @@ helm repo update gitlab
 
 helm search repo -l gitlab/gitlab-runner
 
-# check for existing install if found upgrade existing one, if not found install new one
-helm upgrade gitlab-runner -n gitlab --set "gitlabUrl=https://$GITLABURL,runnerRegistrationToken=$GITLABRUNNERKEY" \
-    gitlab/gitlab-runner -f runner/values.yml
-
 # For Helm 3
 helm install -n gitlab gitlab-runner -f runner/values.yml gitlab/gitlab-runner \
-    --set "gitlabUrl=$GITLABURL,runnerRegistrationToken=$GITLABRUNNERKEY"
+    --set "runnerRegistrationToken=$GITLABRUNNERKEY"
 
 helm upgrade --install -n gitlab gitlab-runner -f runner/values.yml gitlab/gitlab-runner \
-    --set "gitlabUrl=$GITLABURL,runnerRegistrationToken=$GITLABRUNNERKEY"
+    --set "runnerRegistrationToken=$GITLABRUNNERKEY"
 
